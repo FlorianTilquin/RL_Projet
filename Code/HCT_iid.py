59b7b3108674e1f77b616a941e315ab3b3a21ef8
Florian Tilquin <ftilquin@gmail.com>
2015-12-16 17:00:26 +0100

    Ton code pour HCT

---
+++ Code/HCT_iid.py
@@ -0,0 +1,40 @@
+import numpy as np
+import numpy.random as npr
+
+ro = 0.5
+v1 = 1.0
+c1 = (ro/(3*v1))**(1.0/8)
+c = 2*(ro/(1.0-ro))**(0.5)
+
+def HCT():
+    t = 1
+    covTree = [[0,1],[1,1],[1,2]]
+    H = 1
+    U = ##a remplir upper bounds
+    T = ##a remplir counts
+    if(t=plus(t)):
+        ##refresh loop
+        
+    [h,i],P = OptTraverse(covTree)
+    if(type=='iid'):
+        x,r = 
+        t+=1
+    if(type=='gamma'):
+        ##coder
+    T = update_T()
+    mu = update_mu()
+    U = update_U()
+    to = 
+    if( and ):
+        
+    
+    return
+    
+def index(h,i):
+    return 2**(h-1)+(i-1)
+    
+def plus(t):
+    return 2**(np.floor(np.log(t))+1)
+    
+def OptTraverse():
+    return 0
